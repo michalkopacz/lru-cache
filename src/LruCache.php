b186ad5eebdd08c4dac42d152bd95c2734314f42
Michal Kopacz <kangurmk@gmail.com>
2014-11-15 20:43:32 +0100

    init

---
+++ src/LruCache.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MichalKopacz\LruCache;
+
+/**
+ * @author Michał Kopacz
+ */
+class LruCache
+{
+    protected $limit;
+
+    public function __construct($limit)
+    {
+        $this->limit = $limit;
+    }
+
+    public function get($key)
+    {
+
+    }
+
+    public function set($key, $value)
+    {
+
+    }
+
+    public function remove($key)
+    {
+
+    }
+} 
