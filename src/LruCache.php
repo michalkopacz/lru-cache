4fa2375ba8a737ce5060ed8d38313c64ea64ea29
Michal Kopacz <kangurmk@gmail.com>
2014-11-15 22:20:27 +0100

    lru implementation with tests

--- src/LruCache.php
+++ src/LruCache.php
@@ -3,29 +3,75 @@
 namespace MichalKopacz\LruCache;
 
 /**
+ * This cache use 
+ *
  * @author Michał Kopacz
  */
 class LruCache
 {
-    protected $limit;
+    protected $size;
 
-    public function __construct($limit)
+    /**
+     * Element at the end of array is last used, at the beginning is earliest used.
+     *
+     * @var array
+     */
+    protected $data = array();
+
+    public function __construct($size)
     {
-        $this->limit = $limit;
+        if (!is_int($size) || (int)$size <= 0) {
+            throw new \InvalidArgumentException('Size must be positive integer');
+        }
+
+        $this->size = (int)$size;
     }
 
     public function get($key)
     {
+        if (!array_key_exists($key, $this->data)) {
+            throw new \InvalidArgumentException('Key not exists');
+        }
 
+        $value = $this->data[$key];
+
+        $this->changeKeyToLastUsed($key, $value);
+
+        return $value;
     }
 
     public function set($key, $value)
     {
+        if (!is_string($key) && !is_int($key)) {
+            throw new \InvalidArgumentException('Only string and integer is valid key type');
+        }
 
+        if (array_key_exists($key, $this->data)) {
+            $this->changeKeyToLastUsed($key, $value);
+            return;
+        }
+
+        if (count($this->data) === $this->size) {
+            $this->removeEarliestUsedKey();
+        }
+
+        $this->data[$key] = $value;
     }
 
     public function remove($key)
     {
-
+        unset($this->data[$key]);
     }
-} 
+
+    protected function removeEarliestUsedKey()
+    {
+        array_shift($this->data);
+    }
+
+    protected function changeKeyToLastUsed($key, $value)
+    {
+        unset($this->data[$key]);
+
+        $this->data[$key] = $value;
+    }
+}
---
+++ tests/unit/LruCacheTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace MichalKopacz\LruCache\Tests;
+
+use MichalKopacz\LruCache\LruCache;
+
+class LruCacheTest extends  \PHPUnit_Framework_TestCase
+{
+    protected $lruCache;
+
+    public function setUp()
+    {
+        $this->lruCache = new LruCache(3);
+    }
+
+    public function testCanSetAndGetValue()
+    {
+        $this->lruCache->set('foo', 'bar');
+
+        $this->assertSame('bar', $this->lruCache->get('foo'));
+    }
+
+    /**
+     * @dataProvider invalidKeysProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetInvalidKey($key)
+    {
+        $this->lruCache->set($key, 'bazz');
+    }
+
+    public function testCanOverwriteExistingKey()
+    {
+        $this->lruCache->set('foo', 'bar');
+        $this->lruCache->set('foo', 'baz');
+
+        $this->assertSame('baz', $this->lruCache->get('foo'));
+    }
+
+    /**
+     * @dataProvider invalidCacheSizeProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCreateCacheWithInvalidSize($key)
+    {
+        new LruCache($key);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testKeyValueIsNotSet()
+    {
+        $value = $this->lruCache->get('foo');
+    }
+
+    public function testOverflowCacheRemoveEarliestUsedKey()
+    {
+        $this->lruCache->set('foo', 1);
+        $this->lruCache->set('bar', 2);
+        $this->lruCache->set('baz', 3);
+        $this->lruCache->set('buz', 4);
+
+        try {
+            $this->lruCache->get('foo');
+
+            throw new \Exception('Failed asserting that exception of type "\InvalidArgumentException" is thrown.');
+        } catch (\InvalidArgumentException $e) {}
+
+        $this->assertSame(2, $this->lruCache->get('bar'));
+        $this->assertSame(3, $this->lruCache->get('baz'));
+        $this->assertSame(4, $this->lruCache->get('buz'));
+
+    }
+
+    public function testSetTheSameKeyNotRemoveEarliestUsedKey()
+    {
+        $this->lruCache->set('foo', 1);
+        $this->lruCache->set('baz', 2);
+        $this->lruCache->set('bar', 3);
+        $this->lruCache->set('baz', 4);
+
+        $this->assertSame(1, $this->lruCache->get('foo'));
+        $this->assertSame(4, $this->lruCache->get('baz'));
+        $this->assertSame(3, $this->lruCache->get('bar'));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testGetEarliestUsedKeyIsRemoved()
+    {
+        $this->lruCache->set('foo', 1);
+        $this->lruCache->set('bar', 2);
+        $this->lruCache->set('baz', 3);
+
+        $this->lruCache->get('foo');
+
+        $this->lruCache->set('buz', 4);
+
+        $this->lruCache->get('bar');
+    }
+
+    public function invalidKeysProvider()
+    {
+        return array(
+            array(null),
+            array(false),
+            array(true),
+            array(array()),
+            array(new \stdClass()),
+            array(1.23),
+        );
+    }
+
+    public function invalidCacheSizeProvider()
+    {
+        return array(
+            array(null),
+            array(false),
+            array(true),
+            array(array()),
+            array(new \stdClass()),
+            array(1.23),
+            array(0),
+            array(-1),
+            array(-1234),
+        );
+    }
+}
